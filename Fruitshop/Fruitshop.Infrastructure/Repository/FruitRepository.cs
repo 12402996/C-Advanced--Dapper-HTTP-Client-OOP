751adaf486b54349682b1a4f4772def7bfea88c6
Berke Ozdemir <12402996@student.pxl.be>
2026-05-06 18:22:31 +0200

    Dapper is know.

---
+++ Fruitshop/Fruitshop.Infrastructure/Repository/FruitRepository.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using Fruitshop.Domain;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruitshop.Infrastructure.Repository
+{
+    internal class FruitRepository
+    {
+        private readonly string _connectionString;
+        public FruitRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public IEnumerable<Fruit> LoadAllFruits()
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+                return connection.Query<Fruit>("Select * FROM Fruits");
+        }
+
+        public bool Delete (int id)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                int rowsAffected = connection.Execute
+                    (
+                        "DELETE FROM Fruits WHERE Id = @id", new { id }
+                    );
+                return rowsAffected > 0;
+            }
+        }
+    }
+}
